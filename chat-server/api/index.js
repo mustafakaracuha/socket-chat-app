38998a99f02122fc01619f850e46235c338240a4
mustafakaracuha <mustafa.karacuha@detaysoft.com>
2024-06-24 14:48:09 +0300

    update

--- chat-server/api/index.js
+++ chat-server/api/index.js
@@ -1,36 +1,27 @@
-const http = require("http");
 const { Server } = require("socket.io");
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("okay");
-});
+module.exports = (req, res) => {
+    if (!res.socket.server.io) {
+        console.log("Socket.IO server is starting...");
+        const io = new Server(res.socket.server);
+        res.socket.server.io = io;
 
-const io = new Server(server, {
-    cors: {
-        origin: "*",
-        methods: ["GET", "POST"],
-        credentials: true,
-    },
-});
+        io.on("connection", (socket) => {
+            console.log("New client connected");
 
-io.on("connection", (socket) => {
-    console.log("New client connected");
+            socket.on("chat message", (msg) => {
+                const messageObject = {
+                    user: msg.user,
+                    text: msg.text,
+                    date: new Date().toLocaleString(),
+                };
+                io.emit("chat message", messageObject);
+            });
 
-    socket.on("chat message", (msg) => {
-        const messageObject = {
-            user: msg.user,
-            text: msg.text,
-            date: new Date().toLocaleString(),
-        };
-        io.emit("chat message", messageObject);
-    });
-
-    socket.on("disconnect", () => {
-        console.log("Client disconnected");
-    });
-});
-
-server.listen(process.env.PORT || 3000, () => {
-    console.log("Socket.IO server is running...");
-});
+            socket.on("disconnect", () => {
+                console.log("Client disconnected");
+            });
+        });
+    }
+    res.end();
+};
