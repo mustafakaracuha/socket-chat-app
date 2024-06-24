007bcea930a04daee70a3cfaedffce7266174109
mustafakaracuha <mustafa.karacuha@detaysoft.com>
2024-06-24 17:46:25 +0300

    update

--- chat-server/api/index.js
+++ chat-server/api/index.js
@@ -1,35 +1,51 @@
 const { Server } = require("socket.io");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
-export default function handler(req, res) {
-    if (!res.socket.server.io) {
-        console.log("Socket.IO server is starting...");
+const httpServer = require("http").createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("okay");
+});
 
-        const io = new Server(res.socket.server, {
-            path: "/api/socket.io",
-            cors: {
-                origin: "*",
-                methods: ["GET", "POST"],
-            },
-        });
+const io = new Server(httpServer, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+    },
+});
 
-        res.socket.server.io = io;
+// Socket.IO bağlantıları için eventler
+io.on("connection", (socket) => {
+    console.log("New client connected");
 
-        io.on("connection", (socket) => {
-            console.log("New client connected");
+    socket.on("chat message", (msg) => {
+        const messageObject = {
+            user: msg.user,
+            text: msg.text,
+            date: new Date().toLocaleString(),
+        };
+        io.emit("chat message", messageObject);
+    });
 
-            socket.on("chat message", (msg) => {
-                const messageObject = {
-                    user: msg.user,
-                    text: msg.text,
-                    date: new Date().toLocaleString(),
-                };
-                io.emit("chat message", messageObject);
-            });
+    socket.on("disconnect", () => {
+        console.log("Client disconnected");
+    });
+});
 
-            socket.on("disconnect", () => {
-                console.log("Client disconnected");
-            });
-        });
+// HTTP proxy middleware oluşturma
+const apiProxy = createProxyMiddleware("/api/socket.io", {
+    target: "https://react-socket-chats.vercel.app",
+    ws: true,
+    changeOrigin: true,
+    pathRewrite: {
+        "^/api/socket.io": "/api/socket.io",
+    },
+});
+
+module.exports = (req, res) => {
+    if (req.url.startsWith("/api/socket.io")) {
+        apiProxy(req, res);
+    } else {
+        // Diğer durumlarda normal işlem
+        httpServer(req, res);
     }
-    res.end();
-}
+};
