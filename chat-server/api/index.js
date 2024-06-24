c0b063ba00fc76a8fb7680df050756e98d1f9702
mustafakaracuha <mustafa.karacuha@detaysoft.com>
2024-06-24 15:34:30 +0300

    update

--- chat-server/api/index.js
+++ chat-server/api/index.js
@@ -10,6 +10,8 @@ const io = new Server(server, {
     cors: {
         origin: "*",
         methods: ["GET", "POST"],
+        allowedHeaders: ["my-custom-header"],
+        credentials: true,
     },
 });
 
